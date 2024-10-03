b382018bdfd121bc0171bfe89d39e469dce8ec00
Fernanda <fernanda.moura.cruz98@gmail.com>
2024-10-02 22:03:06 -0300

    Update

--- project/src/components/Card.tsx
+++ project/src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../assets/css/card.module.css'
 import { FaRegHeart } from "react-icons/fa";
 
@@ -10,13 +10,18 @@ type CardProps = {
 }
 
 export default function Card({date, title, description}:CardProps){
+
+    function handleClick() {
+        alert('You clicked me!');        
+    }
+    
     return(
         <>  
             <div className={styles.card}>
                 <div className={styles.card_container}>
 
                     <div className={styles.info_card}>
-                        <p className={styles.date}>{date}</p> <FaRegHeart className={styles.icon}/>
+                        <p className={styles.date}>{date}</p> <FaRegHeart onClick={handleClick} className={styles.icon}/>
                     </div>
                     <div className={styles.content_card}>
                         <h2>{title}</h2>
