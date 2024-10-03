35012de8675a438d43fc91e6881b93a22bd89d3c
Fernanda <fernanda.moura.cruz98@gmail.com>
2024-10-02 22:31:55 -0300

    Update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import styles from '../assets/css/card.module.css'
 import { FaRegHeart } from "react-icons/fa";
 
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,4 @@
-import React from 'react';
 import styles from '../assets/css/searchBar.module.css';
-
-
-type SearchProps = {
-    input:string;
-    placeholder: string;
-}
 
 
 export default function SearchBar(){
